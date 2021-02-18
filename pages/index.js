09ff3e8fef895c45f309b807cab3aa0d1bdf813a
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-02-18 12:09:38 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Head>
+        <title>Self Intro</title>
+      </Head>
+      <h1>main</h1>
+      {/* <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer> */}
+    </div>
+  );
+}
